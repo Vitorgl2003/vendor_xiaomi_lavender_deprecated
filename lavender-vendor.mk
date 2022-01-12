a3300b508a31735993fc1af3ed633f16885a64c7
ImPrashantt <prashant33968@gmail.com>
2022-01-12 15:13:19 +0000

    lavender: drop QTI datastatusnotification
    This isn't needed anymore on a12 and causing issue with dual sim.
    
    Signed-off-by: ImPrashantt <prashant33968@gmail.com>

--- lavender-vendor.mk
+++ lavender-vendor.mk
@@ -1750,7 +1750,6 @@ PRODUCT_PACKAGES += \
     TimeService \
     PowerOffAlarm \
     QtiTelephonyService \
-    datastatusnotification \
     imssettings \
     uceShimService \
     dpmserviceapp \
